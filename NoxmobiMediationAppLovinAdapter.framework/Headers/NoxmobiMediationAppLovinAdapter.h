0124b7e2f1d5abe1480896f80b9201bf629a5d97
nox_chong <gaochong@bignox.com>
2020-08-03 20:25:12 +0800

    6.13.1.0

---
+++ NoxmobiMediationAppLovinAdapter.framework/Headers/NoxmobiMediationAppLovinAdapter.h
@@ -0,0 +1,14 @@
+//
+//	NoxmobiMediationAppLovinAdapter.h
+//	
+//	▄▄︻┻━━━┳━═━━   BUG~~
+//	
+//  Created by Chong on 2020/7/31.
+//  Copyright © 2020 Chong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define NoxmobiMediationAppLovinAdapterVersion @"6.13.1.0"
+
+
